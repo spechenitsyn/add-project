529f0986d1d8939367cb3efba12be5326cf913a7
Sergey Pechenitsyn <sergeyp1401@gmail.com>
2023-01-24 20:47:59 +0500

    added func

--- new file.py
+++ new file.py
@@ -1,7 +1,15 @@
+def func(i):
+    if i > 5:
+        print('100')
+    else:
+        print('200')
+
 i = 5
 for i in range(10):
     i += 2
     if i == 7:
         continue
     print(i)
+    
+func(i)
 print('Цикл закончен')
